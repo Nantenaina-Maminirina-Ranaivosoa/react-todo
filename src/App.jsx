c5a142df6d970bc3aec477e851462f428635c1b5
NanteRanaivosoa <rnantenainamaminirina@gmail.com>
2025-08-04 16:40:27 +0300

    feat: manage input and tasks list with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "Apprendre React" },
+    { id: 2, text: "Creer une To-do List" },
+  ]);
+
+  const [input, setInput] = useState("");
+
   return (
     <div className="todo-container">
       <h1>Ma To-do List</h1>
@@ -9,19 +17,19 @@ function App() {
         <input
           type="text"
           placeholder="Ajouter une nouvelle tache..."
-          autoComplete="off"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
         />
         <button type="submit">Ajouter</button>
       </form>
+
       <ul className="task-list">
-        <li>
-          <span>Apprendre React</span>
+        {tasks.map(task => (
+        <li key={task.id}>
+          <span>{task.text}</span>
           <button className="delete-btn">Supprimer</button>
         </li>
-        <li>
-          <span>Creer une To-do List</span>
-          <button className="delete-btn">Supprimer</button>
-        </li>
+        ))}
       </ul>
     </div>
   );
